92187c029e093dd9f0f1e8d4af09153c075c4d20
Luke Condliffe <aero161.luke@gmail.com>
2026-01-12 10:24:47 +0000

    auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://touching-jackal-7.clerk.accounts.dev",
+      // The hardcoded value is the fallback for local development
+      domain: process.env.CLERK_ISSUER_URL || "https://touching-jackal-7.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
